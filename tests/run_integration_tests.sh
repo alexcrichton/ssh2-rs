774ae751f62af20a73607eb6ce30607618c7f575
Wez Furlong <wez@wezfurlong.org>
2019-07-23 22:28:10 -0700

    Add script for running integration tests
    This makes it possible to run the integration tests without
    requiring that the user change their local ssh configuration.
    This is desirable because some sites have strict controls over
    the local ssh configuration files.
    
    This commit adds a script that spawns a local copy of the ssh
    daemon running on an alternate port with a specific configuration
    that is known to successfully pass the test suite.
    
    This has only been tested on my local macos machine so far.

---
+++ tests/run_integration_tests.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+set -e
+set -x
+
+# This script spawns an ssh daemon with a known configuration so that we can
+# test various functionality against it.
+
+# Tell the tests to use the port number we're using to spawn this server
+export RUST_SSH2_FIXTURE_PORT=8022
+
+cleanup() {
+  # Stop the ssh server
+  kill $(< $SSHDIR/sshd.pid)
+  # Stop local ssh agent
+  kill $SSH_AGENT_PID
+}
+trap cleanup EXIT
+
+# Blow away any prior state and re-configure our test server
+SSHDIR=$(pwd)/tests/sshd
+
+rm -rf $SSHDIR
+mkdir -p $SSHDIR
+
+eval $(ssh-agent -s)
+
+ssh-keygen -t rsa -f $SSHDIR/id_rsa -N "" -q
+chmod 0600 $SSHDIR/id_rsa*
+ssh-add $SSHDIR/id_rsa
+cp $SSHDIR/id_rsa.pub $SSHDIR/authorized_keys
+
+ssh-keygen -f $SSHDIR/ssh_host_rsa_key -N '' -t rsa
+
+for p in /usr/lib/openssh/sftp-server /usr/libexec/sftp-server ; do
+  if test -x $p ; then
+    SFTP=$p
+  fi
+done
+
+cat > $SSHDIR/sshd_config <<-EOT
+AuthorizedKeysFile=$SSHDIR/authorized_keys
+HostKey=$SSHDIR/ssh_host_rsa_key
+PidFile=$SSHDIR/sshd.pid
+Subsystem sftp $SFTP
+UsePAM yes
+X11Forwarding yes
+PrintMotd yes
+PermitTunnel yes
+AllowTcpForwarding yes
+MaxStartups 500
+# Relax modes when the repo is under eg: /var/tmp
+StrictModes no
+EOT
+
+# Start an ssh server
+/usr/sbin/sshd -p $RUST_SSH2_FIXTURE_PORT -f $SSHDIR/sshd_config -E $SSHDIR/sshd.log
+# Give it a moment to start up
+sleep 2
+
+# Run the tests against it
+cargo test --all
+cargo test --features vendored-openssl
