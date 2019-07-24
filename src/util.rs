f8967b396e7e85072d8eb39654e366c1db5f8e73
Wez Furlong <wez@wezfurlong.org>
2019-07-23 18:00:14 -0700

    remove SessionBinding trait
    I'd gradually removed the usage of it while I was hacking through
    with my lifetime changes.  I suppose it could be added back
    but I confess that I removed it because I found it confusing
    and it didn't look like it would fit on my first pass through
    the code.

--- src/util.rs
+++ src/util.rs
@@ -1,7 +1,7 @@
 use std::borrow::Cow;
 use std::path::Path;
 
-use {raw, Error, Session};
+use {raw, Error};
 
 #[doc(hidden)]
 pub trait Binding: Sized {
@@ -9,22 +9,6 @@ pub trait Binding: Sized {
 
     unsafe fn from_raw(raw: Self::Raw) -> Self;
     fn raw(&self) -> Self::Raw;
-}
-
-#[doc(hidden)]
-pub trait SessionBinding<'sess>: Sized {
-    type Raw;
-
-    unsafe fn from_raw(sess: &'sess Session, raw: *mut Self::Raw) -> Self;
-    fn raw(&self) -> *mut Self::Raw;
-
-    unsafe fn from_raw_opt(sess: &'sess Session, raw: *mut Self::Raw) -> Result<Self, Error> {
-        if raw.is_null() {
-            Err(Error::last_error(sess).unwrap_or_else(Error::unknown))
-        } else {
-            Ok(SessionBinding::from_raw(sess, raw))
-        }
-    }
 }
 
 #[cfg(unix)]
